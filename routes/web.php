d9803fd97a0805a71eb4a164cfd3941f9e476e50
Antonyzer83 <antony.castaner@ynov.com>
2020-01-07 15:44:23 +0100

    Add UserController

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users', 'UserController@index');
+
+Route::get('user/{id}', 'UserController@show')->where('id', '[0-9]+');
