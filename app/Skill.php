c8335850db21a30b92260f685677b748878a7979
Antonyzer83 <antony.castaner@ynov.com>
2020-01-14 11:28:22 +0100

    Add skill migrations and update Controller

--- app/Skill.php
+++ app/Skill.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Skill extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'description'
+    ];
+
+    /**
+     * The users that belong to the skill
+     */
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
 }
